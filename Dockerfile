f10312f787221cc97a02a2a682119438da380884
fshjar002 <fshjar002@myuct.ac.za>
2021-08-19 21:41:18 +0200

    Added comments to code on both docker and wrapper files.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM ubuntu:20.10 AS base
 
-
-FROM base as builder
+#We actually do not need to give the builder part to the client. We only need to give the CarlSat executable
+#This would obviously neccessitate changes to the dockerfile in later stages of development.
+FROM base as builder        
 WORKDIR /app/src/
 
 RUN apt update -y && apt -y install clang-11 lldb-11 lld-11 && apt -y install clang && apt -y install util-linux
@@ -23,9 +24,13 @@ WORKDIR /app/
 RUN apt update -y && apt -y install python3 && apt -y install pip
 RUN pip install -U pymoo
 
+
 COPY --from=builder /app/src/CarlSAT_2021-main/CarlSAT .
+
+#We will eventually be copying in a folder of wcards I imagine.
 COPY --from=builder /app/src/CarlSAT_2021-main/test1.wcard .
 COPY . .
-CMD ["python3", "src/wrapper.py", "test1.wcard", "2"]
+#Eventually the parameters passed into the wrapper class should be more extensive than just the problem card name.
+CMD ["python3", "src/wrapper.py", "test1.wcard", "2"]   
 
 
--- src/wrapper.py
+++ src/wrapper.py
@@ -7,32 +7,63 @@ import tempfile
 import os
 import sys
 
+
 def Convert(string):
     new_list = list(string.split(" "))
     return new_list
 
-def main():
 
+def main():
 
     filename = sys.argv[1]
     timeout = sys.argv[2]
-    
+
     sLine = './CarlSAT -a 2 -b 4 -c 200 -e 1000 -f 5 -r 30 -x 4 -t ' + timeout + ' -v 2 -z ' + filename
 
+    # The following execution of CarlSat and extraction of console output will most likely be moved to a solver object.
+    #Inevitably the cost and timestamp will also be passed into a pymoo problem object/ function as its objectives
+
     with tempfile.TemporaryFile() as tempf:
+
+        # This runs whatever shell command you put into it and the console output is stored in the temporary file
         proc = subprocess.Popen(Convert(sLine), stdout=tempf)
-        proc.wait()
-        tempf.seek(tempf.tell() - 105)
-        tempf.read
-        for line in tempf:
-            print(line.strip())
-        # for line in tempf:
-        #     print(line.strip())
-       # print(tempf.read())
-        
-    # stream = os.popen('echo Returned output')
-    # output = stream.read().strip()
-    # print(output)
+
+        proc.wait()  # Waiting on child process to finish i.e. waiting until CarlSat is finished and displayed its output
+
+        # Go to the end of the file and then back a bit to just have the last two lines of output left.
+        tempf.seek(tempf.tell() - 27)
+        # From current understanding - it is only the last two lines of the output that is relevant, however maybe for prototype 2 onwards
+        # We want the cost and time stamps for every flip - not just the final best cost and time stamp?
+
+
+
+        # Convert bytes into string
+        stringLine = str(tempf.readline(), 'utf-8')
+
+
+        # Find the 'o' character that always comes before the cost value
+        posChar = stringLine.find('o')
+
+
+        # Extract the best cost (in this run of CarlSat)
+        cost = stringLine[(posChar + 2):(len(stringLine) - 1)]
+        print(cost)
+
+        stringLine = str(tempf.readline(), 'utf-8')
+
+        # Find the ':' character that always comes before the time stamp value
+        posChar = stringLine.find(':')
+
+
+        # Extract the time stamp of when the best cost was found (in this run of CarlSat)
+        timeTaken = stringLine[(posChar + 2):(len(stringLine) - 3)]
+
+
+        # Convert the timeTaken into its millisecond representation
+        timeTakenMs = eval(timeTaken) * 1000
+        print(timeTakenMs)
+
+
 
 if __name__ == "__main__":
     main()
